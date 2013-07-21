8b9e64a6eb0f47504a5446f9c3fec42d8418e6ed
bendyorke <benyorke@me.com>
2013-07-21 18:55:07 -0500

    added the ability to call descriptiona dn title on concert

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,3 +1,15 @@
 class Concert < ActiveRecord::Base
 	attr_accessible :kind, :artists, :location, :url
+
+	def description
+		self.kind + " @" + self.location + " featuring " + self.artists + ".  <a href=\"" + self.url + "\">Click Here to find out more.</a>"
+	end
+
+	def title
+		self.artists 
+	end
+
+	def photo
+		http://upload.wikimedia.org/wikipedia/commons/4/4e/Justice_in_concert.jpg
+	end
 end
