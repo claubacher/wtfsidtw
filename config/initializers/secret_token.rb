afcc630683e68ea5aadf5f80108590cf8cf7a21b
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-21 11:44:51 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wtfsidtw::Application.config.secret_token = 'ea0ffa4a86ca7e9bcf4ee276434223497caa8a8c4d16a7e98cea9dfbca4e43f5fa4576fdda0fc33f77f6e814726abed0c256569c1a697d290335677e093402dd'
