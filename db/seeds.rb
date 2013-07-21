e2e494bc1c1e9c0947900ced11c5f6ad3f7f774b
ayoformayo <jamescarney10@gmail.com>
2013-07-21 17:36:57 -0500

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,18 +8,23 @@ end
 city_ids.each do |city|
   groupon = open('http://api.groupon.com/v2/deals.json?client_id=69481c71e6487f01ac963838804ee201cd5efcc9&division_id='+city)
   data = JSON.parse(groupon.read)
-  data['deals'].each { |deal| Deal.create(
-    location: city,
-    title: deal["title"],
-    pitch_html: deal["pitchHtml"],
-    deal_url: deal['dealUrl'],
-    redemption_location: deal["redemptionLocation"],
-    large_image_url: deal["merchant"],
-    starts_at: deal["startAt"],
-    deal_types: deal["dealTypes"],
-    tags: deal["tags"],
-    option_array: deal["options"],
-    announcement_title: deal['announcementTitle'],
-    end_date: deal['endAt'])}
+  data['deals'].each do |deal|
+    tags = deal['tags'].map(&:values).join(" ")
+    if GROUPON_PARENT_TAGS.include? tags
+      Deal.create(
+      location: city,
+      title: deal["title"],
+      pitch_html: deal["pitchHtml"],
+      deal_url: deal['dealUrl'],
+      redemption_location: deal["redemptionLocation"],
+      large_image_url: deal["merchant"],
+      starts_at: deal["startAt"],
+      deal_types: deal["dealTypes"],
+      tags: deal['tags'].map(&:values).join(" "),
+      option_array: deal["options"],
+      announcement_title: deal['announcementTitle'],
+      end_date: deal['endAt'])
+    end
   end
+end
 
