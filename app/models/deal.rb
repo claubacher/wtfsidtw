75c500cdcf1b4952e0ea64ef03defa15693d312f
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-21 17:14:46 -0500

    add all deals in seed and update deal model and migration

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 
 
 class Deal < ActiveRecord::Base
-  attr_accessible :title, :pitch_html, :deal_url, :redemption_location, :website_url, :large_image_url, :starts_at, :deal_types, :tags, :price, :buy_url, :geolocation, :address, :start_date, :end_date, :option_array, :announcement_title
+  attr_accessible :title, :pitch_html, :deal_url, :redemption_location, :website_url, :large_image_url, :starts_at, :deal_types, :tags, :price, :buy_url, :location, :geolocation, :address, :start_date, :end_date, :option_array, :announcement_title
   attr_reader :data
   # def initialize(deal)
     
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,25 @@
+division_ids = open('http://api.groupon.com/v2/divisions.json?client_id=69481c71e6487f01ac963838804ee201cd5efcc9')
+city_data = JSON.parse(division_ids.read)
+city_ids = []
+city_data['divisions'].each do |city|
+  city_ids << city['id']
+end
 
-groupon = open('http://api.groupon.com/v2/deals.json?client_id=69481c71e6487f01ac963838804ee201cd5efcc9&division_id=chicago')
-    data = JSON.parse(groupon.read)
-    data['deals'].each { |deal| Deal.create(
-      title: deal["title"],
-      pitch_html: deal["pitchHtml"],
-      deal_url: deal['dealUrl'],
-      redemption_location: deal["redemptionLocation"],
-      large_image_url: deal["merchant"],
-      starts_at: deal["startAt"],
-      deal_types: deal["dealTypes"],
-      tags: deal["tags"],
-      option_array: deal["options"],
-      announcement_title: deal['announcementTitle'],
-      end_date: deal['endAt'])}
+city_ids.each do |city|
+  groupon = open('http://api.groupon.com/v2/deals.json?client_id=69481c71e6487f01ac963838804ee201cd5efcc9&division_id='+city)
+  data = JSON.parse(groupon.read)
+  data['deals'].each { |deal| Deal.create(
+    location: city,
+    title: deal["title"],
+    pitch_html: deal["pitchHtml"],
+    deal_url: deal['dealUrl'],
+    redemption_location: deal["redemptionLocation"],
+    large_image_url: deal["merchant"],
+    starts_at: deal["startAt"],
+    deal_types: deal["dealTypes"],
+    tags: deal["tags"],
+    option_array: deal["options"],
+    announcement_title: deal['announcementTitle'],
+    end_date: deal['endAt'])}
+  end
+
