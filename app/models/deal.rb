403314bcafedb498fde84388a4c53190b8c0d28b
ayoformayo <jamescarney10@gmail.com>
2013-07-21 16:24:44 -0500

    Update deal.rb

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -5,10 +5,7 @@ require 'open-uri'
 class Deal
   attr_accessor :title, :pitch_html, :deal_url, :redemption_location, :website_url, :large_image_url, :starts_at, :deal_types, :tags, :price, :buy_url, :geolocation, :address, :start_date, :end_date, :option_array, :announcement_title
   attr_reader :data
-  def initialize()
-    groupon = open('http://api.groupon.com/v2/deals.json?client_id=69481c71e6487f01ac963838804ee201cd5efcc9&division_id=chicago')
-    data = JSON.parse(groupon.read)
-    deal = data['deals'].sample
+  def initialize(deal)
     self.title = deal["title"]
     self.pitch_html = deal["pitchHtml"]
     self.deal_url = deal['dealUrl']
@@ -28,3 +25,6 @@ class Deal
 
 end
 
+# groupon = open('http://api.groupon.com/v2/deals.json?client_id=69481c71e6487f01ac963838804ee201cd5efcc9&division_id=chicago')
+#     data = JSON.parse(groupon.read)
+#     deal = data['deals'].sample
