c9411a68642272ec17803e6eb213adcf0183bbc7
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-21 16:47:54 -0500

    add groupon migration and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+groupon = open('http://api.groupon.com/v2/deals.json?client_id=69481c71e6487f01ac963838804ee201cd5efcc9&division_id=chicago')
+    data = JSON.parse(groupon.read)
+    data['deals'].each { |deal| Deal.create(
+      title: deal["title"],
+      pitch_html: deal["pitchHtml"],
+      deal_url: deal['dealUrl'],
+      redemption_location: deal["redemptionLocation"],
+      large_image_url: deal["merchant"],
+      starts_at: deal["startAt"],
+      deal_types: deal["dealTypes"],
+      tags: deal["tags"],
+      option_array: deal["options"],
+      announcement_title: deal['announcementTitle'],
+      end_date: deal['endAt'])}
