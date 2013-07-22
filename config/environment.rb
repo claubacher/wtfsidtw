30abc685563f4e855dd3cd32938e41ab84d907bd
Chris <chris@Christophers-MacBook-Pro.local>
2013-07-21 22:59:55 -0500

    secret

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Wtfsidtw::Application.initialize!
-GROUPON_KEY = ENV['GROUPON_KEY']
 
-GROUPON_URL = "http://api.groupon.com/v2/deals.json?client_id="+GROUPON_KEY
+GROUPON_URL = "http://api.groupon.com/v2/deals.json?client_id=69481c71e6487f01ac963838804ee201cd5efcc9"
 
 GROUPON_PARENT_TAGS = ['Arts and Entertainment','Beauty & Spas','Education','Restaurants','Nightlife']
 
