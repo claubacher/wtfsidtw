fcc9855938d58169ff9170f34191d3139b3fc69a
Chris <chris@Christophers-MacBook-Pro.local>
2013-07-22 00:58:32 -0500

    removed underscores from deals table and model

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,5 +1,5 @@
 class Deal < ActiveRecord::Base
-  attr_accessible :title, :pitch_html, :deal_url, :redemption_location, :website_url, :large_image_url, :starts_at, :deal_types, :tags, :price, :buy_url, :location, :geolocation, :address, :start_date, :end_date, :option_array, :announcement_title
+  attr_accessible :title, :pitchHtml, :dealUrl, :redemptionLocation, :websiteUrl, :largeImageUrl, :startsAt, :dealTypes, :tags, :price, :buyUrl, :location, :geolocation, :address, :startDate, :endDate, :optionArray, :announcementTitle
   attr_reader :data
 
   def photo
--- db/migrate/20130721213252_create_deals.rb
+++ db/migrate/20130721213252_create_deals.rb
@@ -3,16 +3,16 @@ class CreateDeals < ActiveRecord::Migration
     create_table :deals do |deal|
       deal.string :location
       deal.string :title
-      deal.text :pitch_html
-      deal.string :deal_url
-      deal.string :redemption_location
-      deal.string :large_image_url
-      deal.datetime :starts_at
-      deal.text :deal_types
+      deal.text :pitchHtml
+      deal.string :dealUrl
+      deal.string :redemptionLocation
+      deal.string :largeImageUrl
+      deal.datetime :startsAt
+      deal.text :dealTypes
       deal.text :tags
-      deal.text :option_array
-      deal.string :announcement_title
-      deal.datetime :end_date
+      deal.text :optionArray
+      deal.string :announcementTitle
+      deal.datetime :endDate
       deal.timestamps
     end
   end
