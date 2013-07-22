9c9d5d904ee75fd2e81d4a807f577286f39f7d9a
Chris <chris@Christophers-MacBook-Pro.local>
2013-07-22 00:37:17 -0500

    added self keyword to deal#photo

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,13 +1,9 @@
-require 'json'
-require 'open-uri'
-
-
 class Deal < ActiveRecord::Base
   attr_accessible :title, :pitch_html, :deal_url, :redemption_location, :website_url, :large_image_url, :starts_at, :deal_types, :tags, :price, :buy_url, :location, :geolocation, :address, :start_date, :end_date, :option_array, :announcement_title
   attr_reader :data
 
   def photo
-    large_image_url.to_s
+    self.large_image_url
   end
 
   def description
