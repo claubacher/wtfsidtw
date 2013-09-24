82a021c7101d16b8f264cba8d9a2b2de518bb677
Ryan Leahy <ryanleahy90@gmail.com>
2013-09-23 20:31:19 -0700

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,13 @@ class ApplicationController < ActionController::Base
     client = Twilio::REST::Client.new twilio_account_sid, twilio_auth_token
 
     response = Twilio::TwiML::Response.new do |r|
-      r.Sms "#{@directions} #{@idea.title} #{@link}"
-    end.text
+      r.Message do |m|
+        m.Media = @idea.photo
+        m.Body = "#{@directions} #{@idea.title} #{@link}"
+      end
+    end
+
+    content_type :xml
+    response.text
   end
 end
