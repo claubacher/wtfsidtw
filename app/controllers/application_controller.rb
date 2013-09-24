cab2df166cc72417d1bb65ad04f0ba608f13b5cd
Ryan Leahy <ryanleahy90@gmail.com>
2013-09-23 19:29:13 -0700

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,11 +28,11 @@ class ApplicationController < ActionController::Base
       @directions = "You should see "
     end
 
-    # TWILIO_ACCOUNT_SID = ENV['TWILIO_ACCOUNT_SID']
-    # TWILIO_AUTH_TOKEN = ENV['TWILIO_AUTH_TOKEN']
-    # TWILIO_NUMBER = ENV['TWILIO_NUMBER']
+    TWILIO_ACCOUNT_SID = ENV['TWILIO_ACCOUNT_SID']
+    TWILIO_AUTH_TOKEN = ENV['TWILIO_AUTH_TOKEN']
+    TWILIO_NUMBER = ENV['TWILIO_NUMBER']
 
-    # client = Twilio::REST::Client.new TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN
+    client = Twilio::REST::Client.new TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN
 
   #   response = Twilio::TwiML::Response.new do |r|
   #     r.Message do |m|
@@ -43,5 +43,5 @@ class ApplicationController < ActionController::Base
 
   #   content_type :xml
   #   response.text
-  # end
+  end
 end
