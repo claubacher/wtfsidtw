c427418136f924f2d8b765c2dcca73bdd52a8e4e
Ryan Leahy <ryanleahy90@gmail.com>
2013-09-23 19:57:31 -0700

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < ActionController::Base
     response = Twilio::TwiML::Response.new do |r|
       r.Message do |m|
         m.Media = @idea.photo
-        m.Body = "{@directions} #{@idea.title} #{@link}"
+        m.Body = "#{@directions} #{@idea.title} #{@link}"
       end
     end
 
